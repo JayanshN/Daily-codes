f836d9d0d100d8933da43825a6420ea2f66532f7
Jayansh Nagar <120237987+JayanshN@users.noreply.github.com>
2024-10-10 10:28:45 +0530

    Time: 27 ms (33.92%), Space: 22.9 MB (75.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,24 +4,19 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
 
-        for(int i=0; i<intervals.size(); i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if (!ans.empty() && end <= ans.back()[1]) {
-            continue;
-            }
-
-            for(int j=i+1; j<intervals.size(); j++){
-                if(intervals[j][0]<=end){
-                    end=max(intervals[j][1],end);
-                }else{
-                    break;
-                }
-            }
-
-            ans.push_back({start,end});
+         for (int i = 0; i < intervals.size(); i++) {
+        // if the current interval does not
+        // lie in the last interval:
+        if (ans.empty() || intervals[i][0] > ans.back()[1]) {
+            ans.push_back(intervals[i]);
         }
+        // if the current interval
+        // lies in the last interval:
+        else {
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+    }
+
         return ans;
     }
 };
